9deb1c8eea301a0e3d908682b395cf5d30bec24e
Bamidev <bamidev@pm.me>
2025-11-18 12:55:44 +0100

    Enable odoo_repl if available

--- commands/shell.nix
+++ commands/shell.nix
@@ -1,8 +1,9 @@
 { odooMajorVersion }:
 ''
+  OPTS="-c wax/odoo.cfg --load=odoo_repl"
   if [ ${toString odooMajorVersion} -lt 10 ]; then
-    wax/venv/bin/python wax/repos/odoo/odoo.py shell -c wax/odoo.cfg ''$@
+    wax/venv/bin/python wax/repos/odoo/odoo.py shell $OPTS ''$@
   else
-    wax/venv/bin/python wax/repos/odoo/odoo-bin shell -c wax/odoo.cfg ''$@
+    wax/venv/bin/python wax/repos/odoo/odoo-bin shell $OPTS --load=odoo_repl ''$@
   fi
 ''
