4b68831c5f6a25733298734544bec538394834d7
Bamidev <bamidev@pm.me>
2026-02-22 11:57:37 +0100

    Use tee to direct odoo logs to both stdout and wax/log/odoo.log simultaneously

--- commands/build-config.nix
+++ commands/build-config.nix
@@ -5,7 +5,6 @@ let
       addons_path = "./wax/addons";
       db_name = config.databaseName or "";
       dbfilter = if config.databaseName != null then "^" + config.databaseName + "$" else null;
-      logfile = "./wax/log/odoo.log";
     };
   };
   odooConfig = lib.attrsets.recursiveUpdate defaultOdooConfig config.odooConfig;
--- commands/run.nix
+++ commands/run.nix
@@ -1,8 +1,8 @@
 { odooMajorVersion, ... }:
 ''
   if [ ${toString odooMajorVersion} -lt 10 ]; then
-    wax/venv/bin/python wax/repos/odoo/odoo.py -c wax/odoo.cfg ''$@
+    wax/venv/bin/python wax/repos/odoo/odoo.py -c wax/odoo.cfg ''$@ 2>&1 | tee -a wax/log/odoo.log
   else
-    wax/venv/bin/python wax/repos/odoo/odoo-bin -c wax/odoo.cfg ''$@
+    wax/venv/bin/python wax/repos/odoo/odoo-bin -c wax/odoo.cfg ''$@ 2>&1 | tee -a wax/log/odoo.log
   fi
 ''
