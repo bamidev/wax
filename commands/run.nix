1dea0e27d43732239c94984a9d0c6c796ab09286
Bamidev <bamidev@pm.me>
2026-06-18 13:07:01 +0200

    Fix run script

--- commands/run.nix
+++ commands/run.nix
@@ -1,12 +1,12 @@
 { pkgs, odooMajorVersion, ... }:
 ''
-  CMD_PREFIX="${pkgs.expect}/bin/unbuffer wax/venv/bin/python wax/repos/odoo/"
-  CMD_POSTFIX="-c wax/odoo.cfg ''$@ 2>&1 | tee -a wax/log/odoo.log"
+  CMD_PREFIX="${pkgs.expect}/bin/unbuffer wax/venv/bin/python wax/repos/odoo"
+  CMD_POSTFIX="-c wax/odoo.cfg"
   if [ ${toString odooMajorVersion} -lt 8 ]; then
-    $CMD_PREFIX/openerp-server $CMD_POSTFIX
-  else if [ ${toString odooMajorVersion} -lt 10 ]; then
-    $CMD_PREFIX/odoo.py $CMD_POSTFIX
+    $CMD_PREFIX/openerp-server $CMD_POSTFIX ''$@ 2>&1 | tee -a wax/log/odoo.log
+  elif [ ${toString odooMajorVersion} -lt 10 ]; then
+    $CMD_PREFIX/odoo.py $CMD_POSTFIX ''$@ 2>&1 | tee -a wax/log/odoo.log
   else
-    $CMD_PREFIX/odoo-bin $CMD_POSTFIX 
+    $CMD_PREFIX/odoo-bin $CMD_POSTFIX  ''$@ 2>&1 | tee -a wax/log/odoo.log
   fi
 ''
